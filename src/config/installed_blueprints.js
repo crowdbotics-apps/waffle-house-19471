ea09818f4cf2d09b645e6568b653baecb98f8131
Crowdbotics <team@crowdbotics.com>
2020-08-09 20:53:53 +0000

    add package blueprint-react-native-contacts-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Contacts189365', human_name: 'Contacts 1', access_route: 'Contacts189365'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
